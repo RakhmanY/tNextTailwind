d09c6841816855c8f9e64808aef866fbd20626f1
RakhmanY <yudazrakhman30@gmail.com>
2021-09-03 15:23:48 +0700

    Section Header

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import 'tailwindcss/tailwind.css';
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@300;400&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap" rel="stylesheet" />
+      </head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp
